ddf971ce837e8bc0c5348dd13fcd1628775049d4
l-Tsukare-l <meteabalajmi@gmail.com>
2022-07-28 14:45:26 +0300

    Updates

---
+++ precache-manifest.fad96612ca0c6bc0857ff675b966501a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "85524d69f28a2d037c16",
+    "url": "/Portfolio/static/css/main.eae5a415.chunk.css"
+  },
+  {
+    "revision": "85524d69f28a2d037c16",
+    "url": "/Portfolio/static/js/main.85524d69.chunk.js"
+  },
+  {
+    "revision": "e583211323c7046e4dc9",
+    "url": "/Portfolio/static/js/1.e5832113.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "eebd44c43947415bf8deec170b964368",
+    "url": "/Portfolio/static/media/CV.eebd44c4.pdf"
+  },
+  {
+    "revision": "7c628e66787162bc630c193687001d4e",
+    "url": "/Portfolio/static/media/engineering.7c628e66.png"
+  },
+  {
+    "revision": "49c53331d19be74b52d47fcce7e97468",
+    "url": "/Portfolio/static/media/User.49c53331.png"
+  },
+  {
+    "revision": "47703b5b6fed21ac6efcb763aa874f29",
+    "url": "/Portfolio/static/media/SAHM2.47703b5b.jpg"
+  },
+  {
+    "revision": "95d4ceabe6270d2733547df5d399face",
+    "url": "/Portfolio/static/media/sudoku.95d4ceab.png"
+  },
+  {
+    "revision": "0018402ae5a48799160ecf5f1ca4d06f",
+    "url": "/Portfolio/index.html"
+  }
+];
